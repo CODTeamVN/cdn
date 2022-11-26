3ff77b007d80364dcf070fb7da90d66271b359bd
TinyHust <7279786+TinyHust@users.noreply.github.com>
2022-11-26 15:11:36 +0700

    Update integrate.js

--- 1644-7484/integrate.js
+++ 1644-7484/integrate.js
@@ -5,212 +5,212 @@ const odClientToken = document.getElementById('od-integrate').getAttribute('attr
 const style = document.createElement('style');
 
 style.textContent = `
-.od-prevent-scroll {
-  overflow: hidden !important;
-  margin: 0 !important;
-  padding: 0 !important;
-}
-.editor-wrap {
-  position: fixed;
-  z-index: -1;
-  pointer-events: none;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  opacity: 0;
-  visibility: hidden;
-  background: #fff;
-  transform: opacity 0.4s;
-  -webkit-transform: opacity 0.4s;
-  -moz-transform: opacity 0.4s;
-}
-.editor-wrap.is-visible {
-  opacity: 1;
-  visibility: visible;
-  z-index: 9999;
-  pointer-events: all;
-  transform: opacity 0.4s;
-  -webkit-transform: opacity 0.4s;
-  -moz-transform: opacity 0.4s;
-}
-.od-editor {
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  z-index: 1;
-}
-.nbd-load-page {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  z-index: 2;
-  background: #fdfdfd;
-}
-.nbd-load-page.is-invisible {
-  z-index: -1;
-  opacity: 0;
-  pointer-events: none;
-}
-.nbd-load-page .loader {
-  position: relative;
-  margin: -50px auto 0 -50px;
-  width: 100px;
-  top: 50%;
-  left: 50%;
-}
-.nbd-load-page .loader:before {
-  content: "";
-  display: block;
-  padding-top: 100%;
-}
-.nbd-load-page .circular {
-  -webkit-animation: od-rotate 2s linear infinite;
-  animation: od-rotate 2s linear infinite;
-  height: 100%;
-  -webkit-transform-origin: center center;
-  transform-origin: center center;
-  width: 100%;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
-}
-.nbd-load-page .circular .path {
-  stroke-dasharray: 1, 200;
-  stroke-dashoffset: 0;
-  -webkit-animation: od-dash 1.5s ease-in-out infinite, od-color 6s ease-in-out infinite;
-  animation: od-dash 1.5s ease-in-out infinite, od-color 6s ease-in-out infinite;
-  stroke-linecap: round;
-}
-@-webkit-keyframes od-rotate {
-  to {
-	-webkit-transform: rotate(1turn);
-	transform: rotate(1turn);
+  .od-prevent-scroll {
+    overflow: hidden !important;
+    margin: 0 !important;
+    padding: 0 !important;
   }
-}
-@keyframes od-rotate {
-  to {
-	-webkit-transform: rotate(1turn);
-	transform: rotate(1turn);
+  .editor-wrap {
+    position: fixed;
+    z-index: -1;
+    pointer-events: none;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    visibility: hidden;
+    background: #fff;
+    transform: opacity 0.4s;
+    -webkit-transform: opacity 0.4s;
+    -moz-transform: opacity 0.4s;
   }
-}
-@-webkit-keyframes od-dash {
-  0% {
-	stroke-dasharray: 1, 200;
-	stroke-dashoffset: 0;
+  .editor-wrap.is-visible {
+    opacity: 1;
+    visibility: visible;
+    z-index: 9999;
+    pointer-events: all;
+    transform: opacity 0.4s;
+    -webkit-transform: opacity 0.4s;
+    -moz-transform: opacity 0.4s;
   }
-  50% {
-	stroke-dasharray: 89, 200;
-	stroke-dashoffset: -35px;
+  .od-editor {
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 1;
   }
-  to {
-	stroke-dasharray: 89, 200;
-	stroke-dashoffset: -124px;
+  .nbd-load-page {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 2;
+    background: #fdfdfd;
   }
-}
-@keyframes od-dash {
-  0% {
-	stroke-dasharray: 1, 200;
-	stroke-dashoffset: 0;
+  .nbd-load-page.is-invisible {
+    z-index: -1;
+    opacity: 0;
+    pointer-events: none;
   }
-  50% {
-	stroke-dasharray: 89, 200;
-	stroke-dashoffset: -35px;
+  .nbd-load-page .loader {
+    position: relative;
+    margin: -50px auto 0 -50px;
+    width: 100px;
+    top: 50%;
+    left: 50%;
   }
-  to {
-	stroke-dasharray: 89, 200;
-	stroke-dashoffset: -124px;
+  .nbd-load-page .loader:before {
+    content: "";
+    display: block;
+    padding-top: 100%;
   }
-}
-@-webkit-keyframes od-color {
-  0%,
-  to {
-	stroke: #d62d20;
+  .nbd-load-page .circular {
+    -webkit-animation: od-rotate 2s linear infinite;
+    animation: od-rotate 2s linear infinite;
+    height: 100%;
+    -webkit-transform-origin: center center;
+    transform-origin: center center;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
   }
-  40% {
-	stroke: #0057e7;
+  .nbd-load-page .circular .path {
+    stroke-dasharray: 1, 200;
+    stroke-dashoffset: 0;
+    -webkit-animation: od-dash 1.5s ease-in-out infinite, od-color 6s ease-in-out infinite;
+    animation: od-dash 1.5s ease-in-out infinite, od-color 6s ease-in-out infinite;
+    stroke-linecap: round;
   }
-  66% {
-	stroke: #008744;
+  @-webkit-keyframes od-rotate {
+    to {
+      -webkit-transform: rotate(1turn);
+      transform: rotate(1turn);
+    }
   }
-  80%,
-  90% {
-	stroke: #ffa700;
+  @keyframes od-rotate {
+    to {
+      -webkit-transform: rotate(1turn);
+      transform: rotate(1turn);
+    }
   }
-}
-@keyframes od-color {
-  0%,
-  to {
-	stroke: #d62d20;
+  @-webkit-keyframes od-dash {
+    0% {
+      stroke-dasharray: 1, 200;
+      stroke-dashoffset: 0;
+    }
+    50% {
+      stroke-dasharray: 89, 200;
+      stroke-dashoffset: -35px;
+    }
+    to {
+      stroke-dasharray: 89, 200;
+      stroke-dashoffset: -124px;
+    }
   }
-  40% {
-	stroke: #0057e7;
+  @keyframes od-dash {
+    0% {
+      stroke-dasharray: 1, 200;
+      stroke-dashoffset: 0;
+    }
+    50% {
+      stroke-dasharray: 89, 200;
+      stroke-dashoffset: -35px;
+    }
+    to {
+      stroke-dasharray: 89, 200;
+      stroke-dashoffset: -124px;
+    }
   }
-  66% {
-	stroke: #008744;
+  @-webkit-keyframes od-color {
+    0%,
+    to {
+      stroke: #d62d20;
+    }
+    40% {
+      stroke: #0057e7;
+    }
+    66% {
+      stroke: #008744;
+    }
+    80%,
+    90% {
+      stroke: #ffa700;
+    }
   }
-  80%,
-  90% {
-	stroke: #ffa700;
+  @keyframes od-color {
+    0%,
+    to {
+      stroke: #d62d20;
+    }
+    40% {
+      stroke: #0057e7;
+    }
+    66% {
+      stroke: #008744;
+    }
+    80%,
+    90% {
+      stroke: #ffa700;
+    }
   }
-}
-.close-editor {
-  top: 12px;
-  left: 15px;
-  position: absolute;
-  width: 30px;
-  height: 30px;
-  background: #f1416c;
-  text-align: center;
-  color: #fff;
-  border-radius: 50%;
-  box-shadow: 0 2px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%);
-  cursor: pointer;
-  font-size: 30px;
-  line-height: 30px;
-  z-index: 3;
-}
-.od-preview {
-  margin: 10px;
-  background: #fff;
-  border-radius: 4px;
-  border: 3px solid #fff;
-  height: 100px;
-  width: auto;
-  box-shadow: 0 0.5rem 1.5rem 0.5rem rgb(0 0 0 / 8%);
-}
+  .close-editor {
+    top: 12px;
+    left: 15px;
+    position: absolute;
+    width: 30px;
+    height: 30px;
+    background: #f1416c;
+    text-align: center;
+    color: #fff;
+    border-radius: 50%;
+    box-shadow: 0 2px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%);
+    cursor: pointer;
+    font-size: 30px;
+    line-height: 30px;
+    z-index: 3;
+  }
+  .od-preview {
+    margin: 10px;
+    background: #fff;
+    border-radius: 4px;
+    border: 3px solid #fff;
+    height: 100px;
+    width: auto;
+    box-shadow: 0 0.5rem 1.5rem 0.5rem rgb(0 0 0 / 8%);
+  }
 
-.od-download {
-  cursor: pointer;
-  margin: 15px 0;
-}
-.od-download {
-  border-collapse: collapse;
-}
-.od-download td, .od-download th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
-.od-none {
-  display: none;
-}
-.od-export-loader {
-  border: 16px solid #f3f3f3;
-  border-radius: 50%;
-  border-top: 16px solid #3498db;
-  width: 60px;
-  height: 60px;
-  -webkit-animation: od-rotate 2s linear infinite;
-  animation: od-rotate 2s linear infinite;
-}
+  .od-download {
+    cursor: pointer;
+    margin: 15px 0;
+  }
+  .od-download {
+    border-collapse: collapse;
+  }
+  .od-download td, .od-download th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+  }
+  .od-none {
+    display: none;
+  }
+  .od-export-loader {
+    border: 16px solid #f3f3f3;
+    border-radius: 50%;
+    border-top: 16px solid #3498db;
+    width: 60px;
+    height: 60px;
+    -webkit-animation: od-rotate 2s linear infinite;
+    animation: od-rotate 2s linear infinite;
+  }
 `;
 
 document.head.appendChild(style);
@@ -218,184 +218,184 @@ document.head.appendChild(style);
 window.editorLoaded = false;
 window.nbOption = null;
 window.showEditor = function() {
-document.querySelector('body').classList.toggle('od-prevent-scroll');
-document.querySelector('.editor-wrap').classList.toggle('is-visible');
-if(!window.editorLoaded){
-  document.querySelector('#editor-form').submit();
-} else {
-  if( !!window.nbOption ){
-	document.getElementById('od-editor').contentWindow.postMessage({
-	  type: 'change_nbo_options',
-	  nbOption: window.nbOption
-	}, '*')
+  document.querySelector('body').classList.toggle('od-prevent-scroll');
+  document.querySelector('.editor-wrap').classList.toggle('is-visible');
+  if(!window.editorLoaded){
+    document.querySelector('#editor-form').submit();
+  } else {
+    if( !!window.nbOption ){
+      document.getElementById('od-editor').contentWindow.postMessage({
+        type: 'change_nbo_options',
+        nbOption: window.nbOption
+      }, '*')
+    }
   }
 }
-}
 window.onCloseEditor = function() {
-document.querySelector('body').classList.toggle('od-prevent-scroll');
-document.querySelector('.editor-wrap').classList.toggle('is-visible');
+  document.querySelector('body').classList.toggle('od-prevent-scroll');
+  document.querySelector('.editor-wrap').classList.toggle('is-visible');
 }
 window.onLoadedEditor = function() {
-window.editorLoaded = true;
-document.querySelector('.nbd-load-page').classList.toggle('is-invisible');
-if( !!window.nbOption ){
-  document.getElementById('od-editor').contentWindow.postMessage({
-	type: 'change_nbo_options',
-	nbOption: window.nbOption
-  }, '*')
-}
+  window.editorLoaded = true;
+  document.querySelector('.nbd-load-page').classList.toggle('is-invisible');
+  if( !!window.nbOption ){
+    document.getElementById('od-editor').contentWindow.postMessage({
+      type: 'change_nbo_options',
+      nbOption: window.nbOption
+    }, '*')
+  }
 }
 window.onSavedDesign = function(path, uuid, numberOfSide) {
-const event = new CustomEvent('onSavedDesign', { detail: {path, uuid, numberOfSide }});
-document.dispatchEvent(event);
+  const event = new CustomEvent('onSavedDesign', { detail: {path, uuid, numberOfSide }});
+  document.dispatchEvent(event);
 
-const previews = document.querySelectorAll('.od-preview');
-previews.forEach(preview => {
-  preview.remove();
-});
+  const previews = document.querySelectorAll('.od-preview');
+  previews.forEach(preview => {
+    preview.remove();
+  });
 
-if( document.getElementById('od-design-uuid') ){
-  document.getElementById('od-design-uuid').remove();
-}
+  if( document.getElementById('od-design-uuid') ){
+    document.getElementById('od-design-uuid').remove();
+  }
 
 
-document.getElementById('od-wrapper').insertAdjacentHTML("afterend", `<input id="od-design-uuid" type="text" value="${uuid}" />`);
-const imgEls = [...Array(numberOfSide).keys()].map(key => `<img class="od-preview" src="${odBaseUrl}/${path}frame_${key}.png" />`);
-document.getElementById('od-start-design-btn').insertAdjacentHTML("afterend", `<div class="od-preview-wrap">${imgEls.join('')}</div>`);
+  document.getElementById('od-wrapper').insertAdjacentHTML("afterend", `<input id="od-design-uuid" type="text" value="${uuid}" />`);
+  const imgEls = [...Array(numberOfSide).keys()].map(key => `<img class="od-preview" src="${odBaseUrl}/${path}frame_${key}.png" />`);
+  document.getElementById('od-start-design-btn').insertAdjacentHTML("afterend", `<div class="od-preview-wrap">${imgEls.join('')}</div>`);
 }
 
 window.addEventListener("message", (event) => {
-if( event.data == 'onLoadedEditor' ){
-  window.onLoadedEditor()
-}
-if( event.data == 'onCloseEditor' ){
-  window.onCloseEditor()
-}
-if( typeof event.data == 'object') {
-  if( event.data.type == 'onSavedDesign' ){
-	window.onSavedDesign(event.data.path, event.data.uuid, parseInt(event.data.numberOfSide));
+  if( event.data == 'onLoadedEditor' ){
+    window.onLoadedEditor()
   }
-}
+  if( event.data == 'onCloseEditor' ){
+    window.onCloseEditor()
+  }
+  if( typeof event.data == 'object') {
+    if( event.data.type == 'onSavedDesign' ){
+      window.onSavedDesign(event.data.path, event.data.uuid, parseInt(event.data.numberOfSide));
+    }
+  }
 }, false);
 
 document.addEventListener("DOMContentLoaded", () => {
-const odWrapper = document.getElementById('od-wrapper');
-const cmsId = odWrapper.getAttribute('attr-pid');
-const odStartDesignBtn = document.getElementById('od-start-design-btn');
-const odHtml = `
-  <form action="http://localhost:3000/product?pid=${cmsId}" target="od-editor" method="post" id="editor-form">
-	<input type="hidden" name="X-API-KEY" value="${odClientToken}" />
-  </form>
-  <div class="editor-wrap">
-	<div class="nbd-load-page">
-	  <div class="loader">
-		<svg class="circular" viewBox="25 25 50 50">
-		  <circle
-			class="path"
-			cx="50"
-			cy="50"
-			r="20"
-			fill="none"
-			stroke-width="2"
-			stroke-miterlimit="10"
-		  />
-		</svg>
-	  </div>
-	</div>
-	<iframe
-	  name="od-editor"
-	  class="od-editor"
-	  id="od-editor"
-	  scrolling="no"
-	  frameborder="0"
-	  noresize="noresize"
-	  allowfullscreen
-	  mozallowfullscreen="true"
-	  webkitallowfullscreen="true"
-	  src="about:blank"
-	></iframe>
-	<div
-	  class="close-editor"
-	  onclick="onCloseEditor()"
-	>×</div>
-  </div>
-`;
-odWrapper.insertAdjacentHTML("afterend", odHtml);
-odWrapper.removeAttribute('style');
+  const odWrapper = document.getElementById('od-wrapper');
+  const cmsId = odWrapper.getAttribute('attr-pid');
+  const odStartDesignBtn = document.getElementById('od-start-design-btn');
+  const odHtml = `
+    <form action="http://localhost:3000/product?pid=${cmsId}" target="od-editor" method="post" id="editor-form">
+      <input type="hidden" name="X-API-KEY" value="${odClientToken}" />
+    </form>
+    <div class="editor-wrap">
+      <div class="nbd-load-page">
+        <div class="loader">
+          <svg class="circular" viewBox="25 25 50 50">
+            <circle
+              class="path"
+              cx="50"
+              cy="50"
+              r="20"
+              fill="none"
+              stroke-width="2"
+              stroke-miterlimit="10"
+            />
+          </svg>
+        </div>
+      </div>
+      <iframe
+        name="od-editor"
+        class="od-editor"
+        id="od-editor"
+        scrolling="no"
+        frameborder="0"
+        noresize="noresize"
+        allowfullscreen
+        mozallowfullscreen="true"
+        webkitallowfullscreen="true"
+        src="about:blank"
+      ></iframe>
+      <div
+        class="close-editor"
+        onclick="onCloseEditor()"
+      >×</div>
+    </div>
+  `;
+  odWrapper.insertAdjacentHTML("afterend", odHtml);
+  odWrapper.removeAttribute('style');
 });
 
 window.downloadOdDesign = function(uuid) {
-if( document.getElementById(`result-${uuid}`) ){
-  document.getElementById(`result-${uuid}`).remove()
-};
-if( document.getElementById(`loader-${uuid}`) ){
-  document.getElementById(`loader-${uuid}`).remove()
-};
+  if( document.getElementById(`result-${uuid}`) ){
+    document.getElementById(`result-${uuid}`).remove()
+  };
+  if( document.getElementById(`loader-${uuid}`) ){
+    document.getElementById(`loader-${uuid}`).remove()
+  };
 
-let result = {};
-document.getElementById(`download-${uuid}`).insertAdjacentHTML("afterend", `<div class="od-export-loader" id="loader-${uuid}"></div>`);
-fetch(`${odApiUrl}/resources/shop/pdf/${uuid}`, {
-  method: "GET",
-	headers: {
-		'Accept': 'application/json',
-		'Content-Type': 'application/json',
-		Authorization: `Bearer ${odClientToken}`
-	}
-})
-.then(function (response) {
-  return response.json();
-}) 
-.then(function (response) {
-  result.PDF = response;
+  let result = {};
+  document.getElementById(`download-${uuid}`).insertAdjacentHTML("afterend", `<div class="od-export-loader" id="loader-${uuid}"></div>`);
+  fetch(`${odApiUrl}/resources/shop/pdf/${uuid}`, {
+    method: "GET",
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${odClientToken}`
+      }
+  })
+  .then(function (response) {
+    return response.json();
+  }) 
+  .then(function (response) {
+    result.PDF = response;
 
-  async function fetchImages() {
-	const [pngResponse, jpgResponse, epsResponse] = await Promise.all(['png', 'jpg', 'eps'].map(type => {
-	  return fetch(`${odApiUrl}/resources/shop/image/${uuid}/${type}`, {
-		method: "GET",
-		  headers: {
-			  'Accept': 'application/json',
-			  'Content-Type': 'application/json',
-			  Authorization: `Bearer ${odClientToken}`
-		  }
-	  })
-	}));
-	const pngs = await pngResponse.json();
-	const jpgs = await jpgResponse.json();
-	const epss = await epsResponse.json();
-	return [pngs, jpgs, epss];
-  }
-  
-  fetchImages().then(([pngs, jpgs, epss]) => {
-	result.PNG = pngs;
-	result.JPG = jpgs;
-	result.EPS = epss;
+    async function fetchImages() {
+      const [pngResponse, jpgResponse, epsResponse] = await Promise.all(['png', 'jpg', 'eps'].map(type => {
+        return fetch(`${odApiUrl}/resources/shop/image/${uuid}/${type}`, {
+          method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${odClientToken}`
+            }
+        })
+      }));
+      const pngs = await pngResponse.json();
+      const jpgs = await jpgResponse.json();
+      const epss = await epsResponse.json();
+      return [pngs, jpgs, epss];
+    }
+    
+    fetchImages().then(([pngs, jpgs, epss]) => {
+      result.PNG = pngs;
+      result.JPG = jpgs;
+      result.EPS = epss;
 
-	const numberOfSide = result.PDF.length;
-	let downloadHtml = `
-	<div id="result-${uuid}">
-	  <table class="od-download">
-	`;
+      const numberOfSide = result.PDF.length;
+      let downloadHtml = `
+      <div id="result-${uuid}">
+        <table class="od-download">
+      `;
 
-	['PDF', 'PNG', 'JPG', 'EPS'].forEach(type => {
-	  downloadHtml += `<tr><th>${type}</th>`;
-	  for(let i = 0; i < numberOfSide; i++) {
-		downloadHtml += `<td><a href="${result[type][i]}" download>Download</a></td>`
-	  }
-	  downloadHtml += '</tr>';
-	});
+      ['PDF', 'PNG', 'JPG', 'EPS'].forEach(type => {
+        downloadHtml += `<tr><th>${type}</th>`;
+        for(let i = 0; i < numberOfSide; i++) {
+          downloadHtml += `<td><a href="${result[type][i]}" download>Download</a></td>`
+        }
+        downloadHtml += '</tr>';
+      });
 
-	downloadHtml += '</table></div>';
+      downloadHtml += '</table></div>';
 
-	document.getElementById(`download-${uuid}`).classList.toggle('od-none');
-	document.getElementById(`loader-${uuid}`).insertAdjacentHTML("afterend", downloadHtml);
-	document.getElementById(`loader-${uuid}`).remove();
-  }).catch(error => {
-	console.log(error);
-	document.getElementById(`download-${uuid}`).classList.toggle('od-none');
+      document.getElementById(`download-${uuid}`).classList.toggle('od-none');
+      document.getElementById(`loader-${uuid}`).insertAdjacentHTML("afterend", downloadHtml);
+      document.getElementById(`loader-${uuid}`).remove();
+    }).catch(error => {
+      console.log(error);
+      document.getElementById(`download-${uuid}`).classList.toggle('od-none');
+    });
+  })
+  .catch(function (error){
+    console.log(error);
+    document.getElementById(`download-${uuid}`).classList.toggle('od-none');
   });
-})
-.catch(function (error){
-  console.log(error);
-  document.getElementById(`download-${uuid}`).classList.toggle('od-none');
-});
 }
