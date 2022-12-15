6a1b78637f2a5363e416c190a6609b4c509ceff7
TinyHust <7279786+TinyHust@users.noreply.github.com>
2022-12-15 19:45:10 +0700

    Update integrate.js

--- 1644-7484/integrate.js
+++ 1644-7484/integrate.js
@@ -5,24 +5,6 @@ const odClientToken = document.getElementById('od-integrate').getAttribute('attr
 const style = document.createElement('style');
 
 style.textContent = `
-  #od-start-design-btn {
-    background: #fff;
-    border-radius: 3px;
-    border: 1px solid #98a3b3;
-    color: #000;
-    width: 186px;
-    display: inline-block;
-    height: 50px;
-    font-weight: bold;
-    font-size: 1.25em;
-    line-height: 48px;
-    text-align: center;
-    margin: 15px 0;
-    cursor: pointer;
-  }
-  #od-start-design-btn:hover {
-    background: #f3f3f3;
-  }
   .od-prevent-scroll {
     overflow: hidden !important;
     margin: 0 !important;
